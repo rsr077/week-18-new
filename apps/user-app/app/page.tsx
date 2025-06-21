bf8521458286dbf0ec7a5de48bc39b3d128db7c1
Rajeev Singh Rajput <yourfriend77777777@gmail.com>
2025-06-21 14:34:13 +0530

    newPr

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -3,8 +3,11 @@
 
 import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation'
-import { authOptions } from "./lib/auth";
+import { 
+  
 
+
+  
 export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
